49f530731f48840f8709eea7961b444482a37185
0y0 <ask@lab.oyo.tokyo>
2020-10-16 09:16:05 +0900

    Sync up

--- assets/js/rss-wall.js
+++ assets/js/rss-wall.js
@@ -1,4 +1,4 @@
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const proxyurl = "https://k34f75nkq2.herokuapp.com/";
 
 const htmlEntities = {
   nbsp: ' ',
@@ -38,9 +38,8 @@ function formatTitle(str) {
 };
 
 function renderArticle(item, recent) {
-  //console.log(item);
   var ts = new Date(item.pubDate);
-  var cl = ts > recent ? ' class="recent"' : '';
+  var cl = (recent && ts > recent) ? ' class="recent"' : '';
   var html = `
     <article${cl}>
       <a href="${item.link}" target="_blank" rel="noopener">
@@ -53,92 +52,72 @@ function renderArticle(item, recent) {
   document.body.insertAdjacentHTML('beforeend', html);
 }
 
-function asyncFetch(items, link) {
-  return fetch(proxyurl + link)
-    .then(response => response.text())
-    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then(data => {
-      var cutoff = offsetDate(-7 * 24); // one week
-      data.querySelectorAll("item").forEach(i => {
-        var image = i.querySelector("image")?.innerHTML;
-        var pubDate = Date.parse(i.querySelector("pubDate")?.innerHTML);
-        if (image && image.indexOf('-thumb.') < 0 && pubDate > cutoff) {
-          items.push({
-            pubDate: pubDate,
-            title: formatTitle(i.querySelector("title")?.innerHTML),
-            image: image,
-            link: i.querySelector("link")?.innerHTML,
-          });
-        }
-      });
-    });
-}
-
-async function fetchRss(links) {
-  var items = [];
-
-  // load from sources
-  for (var url of links) {
-    await asyncFetch(items, url);
-  }
-
-  // order from newest to oldest, remove duplicates
-  items.sort((a, b) => (a.pubDate < b.pubDate) ? 1 : -1);
-  items = items.filter((a, i, self) => i === self.findIndex((t) => (t.title === a.title)));
-
-  // remove splash
-  var splash = document.getElementById("splash");
-  splash.parentNode.removeChild(splash);
-
-  // render to body
-  for (var i of items) {
-    renderArticle(i, offsetDate(-6)); // recent
-  }
-}
-
-function asyncFetchWP(items, link) {
-  return fetch(proxyurl + link)
+function asyncFetch(items, url, cutoff) {
+  return fetch(url)
     .then(response => response.text())
     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
     .then(data => {
       data.querySelectorAll("item").forEach(i => {
         var pubDate = Date.parse(i.querySelector("pubDate")?.innerHTML);
-        var image = i.getElementsByTagName("media:thumbnail")[0]?.getAttribute("url");
-        if (!image) {
-          var desc = i.querySelector("description")?.innerHTML;
-          var html = new DOMParser().parseFromString(desc, "text/html");
-          image = html.querySelector("img")?.getAttribute("src");
-        }
-        if (image) {
-          items.push({
-            pubDate: pubDate,
-            title: formatTitle(i.querySelector("title")?.innerHTML),
-            image: image + '?w=400',
-            link: i.querySelector("link")?.innerHTML,
-          });
+        if (!cutoff || pubDate > cutoff) {
+          // look for a picture
+          var image = i.querySelector("image")?.innerHTML;
+          if (!image) {
+            image = i.getElementsByTagName("media:thumbnail")[0]?.getAttribute("url");
+          }
+          if (!image) {
+            for (var m of i.getElementsByTagName("media:content")) {
+              var url = m.getAttribute("url");
+              if (url.match(/(.jpg|.jpeg|.png|.gif)/i)) {
+                image = url;
+                break;
+              }
+            }
+          }
+          if (!image) {
+            var enc = i.getElementsByTagName("content:encoded")[0]?.innerHTML;
+            var html = new DOMParser().parseFromString(enc, "text/html");
+            image = html.querySelector("img")?.getAttribute("src");
+            if (!image) image = html.querySelector("img")?.getAttribute("file");
+          }
+          if (!image) {
+            var desc = i.querySelector("description")?.innerHTML;
+            var html = new DOMParser().parseFromString(desc, "text/html");
+            image = html.querySelector("img")?.getAttribute("src");
+          }
+          if (image && image.indexOf('-thumb.') < 0) { // skip if no good picture
+            items.push({
+              pubDate: pubDate,
+              title: formatTitle(i.querySelector("title")?.innerHTML),
+              image: image + '?w=400', // try to limit size if supported by source
+              link: i.querySelector("link")?.innerHTML,
+            });
+          }
         }
       });
     });
 }
 
-async function fetchWP(links) {
-  var items = [];
+async function fetchRss(links, hours, local) {
+  if (hours == null) hours = 7 * 24; // default to one week
 
-  // load from sources
+  // load from RSS sources
+  var items = [];
   for (var url of links) {
-    await asyncFetchWP(items, url);
+    var link = local ? url : proxyurl + url;
+    await asyncFetch(items, link, hours == 0 ? null : offsetDate(-hours)); // no limit if hours is zero
   }
 
-  // order from newest to oldest, remove duplicates
+  // order from newest to oldest and remove duplicates
   items.sort((a, b) => (a.pubDate < b.pubDate) ? 1 : -1);
   items = items.filter((a, i, self) => i === self.findIndex((t) => (t.title === a.title)));
 
-  // remove splash
+  // stop splash
   var splash = document.getElementById("splash");
   splash.parentNode.removeChild(splash);
 
   // render to body
   for (var i of items) {
-    renderArticle(i, offsetDate(-24)); // recent
+    renderArticle(i, offsetDate(-24)); // highlight recent articles
   }
 }
